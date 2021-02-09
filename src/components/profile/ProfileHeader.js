1239947b309f687c10f718994323f20cd60c9985
TahanyAljemzawy <tahanyaljemzawy@yahoo.com>
2021-02-09 20:19:15 +0200

    profile-responsiv-ready

--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './ProfileHeader.css'
 import userImg from './userImg.PNG'
 import profileCover from './cover.jpeg'
-
+import {BsCalendar} from 'react-icons/bs'
 
 function ProfileHeader() {
     return (
@@ -13,28 +13,27 @@ function ProfileHeader() {
             
             </div>
             {/* user img + btn follow/unFllow for others || Edit profile for user */}
+            <div className='header_info'>  
             <div className='header_userImg'>
             <Avatar src={userImg} className='userImg'/>
            
             <Button className='update-btn'>Follow</Button>
             </div>
-            {/* user name */}
-            <h3>Tahany Darmusleh</h3>
-            {/* bio */}
-            <p>The life is alweyse hard so don't be like the glass</p>
-            {/* joining day */}
-            <h4>12/12/2021</h4>
-            {/* number of following  +  num of follower */}
-            <div className='followers'>
-                <h4>following 6</h4>
-                <h4>followers 1</h4>
-            </div>
             
-            <div className='header_filter'>
-                <h4>Tweets</h4>
-                <h4> Tweets and replies</h4>
-                <h4> Media</h4>
-                <h4> and Likes</h4>
+                <h3>Sun</h3>
+                <p>The life is alweyse hard so don't be like the glass</p>
+                <h4><BsCalendar /> 12/12/2021</h4>
+                <div className='followers'>
+                    <h4>Following 6</h4>
+                    <h4>Followers 1</h4>
+                </div>
+                
+                <div className='header_filter'>
+                    <h4>Tweets</h4>
+                    <h4> Tweets and replies</h4>
+                    <h4> Media</h4>
+                    <h4> Likes</h4>
+                </div>
             </div>
             {/* Filter choices  */}
         </div>
